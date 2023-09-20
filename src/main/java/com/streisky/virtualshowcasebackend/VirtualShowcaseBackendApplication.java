1e794ba21355a8d7516095bfe4e4ee1b23d72a99
Matheus Streisky <streiskymatheus@gmail.com>
2023-09-20 01:07:00 -0300

    Project upload

---
+++ src/main/java/com/streisky/virtualshowcasebackend/VirtualShowcaseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.streisky.virtualshowcasebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualShowcaseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualShowcaseBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/streisky/virtualshowcasebackend/VirtualShowcaseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.streisky.virtualshowcasebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualShowcaseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
