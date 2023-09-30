571ab2c3d7cbbb3c41b3bfb84631aa827cfcbb19
Matheus Streisky <streiskymatheus@gmail.com>
2023-09-30 06:23:56 -0300

    Controller path fix

--- src/main/java/com/streisky/virtualshowcasebackend/controller/product/ProductController.java
+++ src/main/java/com/streisky/virtualshowcasebackend/controller/product/ProductController.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
-@RequestMapping("products")
+@RequestMapping("/products")
 public class ProductController {
 
     @Autowired
