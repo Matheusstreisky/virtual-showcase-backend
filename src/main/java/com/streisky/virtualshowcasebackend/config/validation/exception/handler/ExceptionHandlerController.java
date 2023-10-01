262fddc6c9ed54dffc9e4946d7e7dfccbdf918ed
Matheus Streisky <streiskymatheus@gmail.com>
2023-10-01 05:40:41 -0300

    Added treatments for authentication and authorization exceptions

--- src/main/java/com/streisky/virtualshowcasebackend/config/validation/exception/handler/ExceptionHandlerController.java
+++ src/main/java/com/streisky/virtualshowcasebackend/config/validation/exception/handler/ExceptionHandlerController.java
@@ -4,8 +4,12 @@ import java.util.List;
 
 import com.streisky.virtualshowcasebackend.config.validation.exception.dto.ErrorDTO;
 import com.streisky.virtualshowcasebackend.config.validation.exception.dto.ValidationErrorDTO;
+import com.streisky.virtualshowcasebackend.exception.TokenCreationFailedException;
+import com.streisky.virtualshowcasebackend.exception.TokenInvalidException;
 import com.streisky.virtualshowcasebackend.exception.VirtualShowcaseNotFoundException;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -23,8 +27,32 @@ public class ExceptionHandlerController {
 
     @ResponseStatus(code = HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public List<ValidationErrorDTO> handleBadRequestError(MethodArgumentNotValidException ex) {
+    public List<ValidationErrorDTO> handleValidationError(MethodArgumentNotValidException ex) {
         List<FieldError> errors = ex.getFieldErrors();
         return errors.stream().map(ValidationErrorDTO::new).toList();
+    }
+
+    @ResponseStatus(code = HttpStatus.UNAUTHORIZED)
+    @ExceptionHandler(AuthenticationException.class)
+    public ErrorDTO handleAuthenticationError() {
+        return new ErrorDTO("Authentication failed.");
+    }
+
+    @ResponseStatus(code = HttpStatus.FORBIDDEN)
+    @ExceptionHandler(AccessDeniedException.class)
+    public ErrorDTO handleAccessDeniedError() {
+        return new ErrorDTO("Access denied.");
+    }
+
+    @ResponseStatus(code = HttpStatus.FORBIDDEN)
+    @ExceptionHandler({TokenCreationFailedException.class, TokenInvalidException.class})
+    public ErrorDTO handleTokenJWTError(Exception ex) {
+        return new ErrorDTO(ex.getMessage());
+    }
+
+    @ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    public ErrorDTO handleUnexpectedError(Exception ex) {
+        return new ErrorDTO("Error: " + ex.getMessage());
     }
 }
